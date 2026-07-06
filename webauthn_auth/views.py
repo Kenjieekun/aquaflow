6f1eb057c0a3699e0948053e62d57bca17393765
Kenjie <danrebablay23@gmail.com>
2026-07-06 15:21:35 +0800

    Implement WebAuthn credential storage

--- webauthn_auth/views.py
+++ webauthn_auth/views.py
@@ -1,4 +1,5 @@
 import json
+import base64
 
 from webauthn.helpers import options_to_json
 
@@ -8,6 +9,8 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
+from webauthn import verify_registration_response
+from water_refilling_system.firebase_admin import db
 
 @csrf_exempt
 @require_POST
@@ -54,6 +57,32 @@ def begin_registration(request):
 @csrf_exempt
 @require_POST
 def finish_registration(request):
+    body = json.loads(request.body)
+
+    firebase_uid = request.session["firebase_uid"]
+
+    challenge = request.session["registration_challenge"]
+
+    verification = verify_registration_response(
+        credential=body,
+        expected_challenge=bytes.fromhex(challenge),
+        expected_origin="https://kenjie.pythonanywhere.com",
+        expected_rp_id="kenjie.pythonanywhere.com",
+        require_user_verification=True,
+    )
+
+    db.collection("webauthn_credentials").document(firebase_uid).set({
+        "credential_id": base64.b64encode(
+            verification.credential_id
+        ).decode(),
+
+        "public_key": base64.b64encode(
+            verification.credential_public_key
+        ).decode(),
+
+        "sign_count": verification.sign_count,
+    })
+
     return JsonResponse({
         "success": True
     })
